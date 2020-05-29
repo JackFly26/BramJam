c3c7efbb19951ec380d528a7a94cc526607276df
JackFly26 <jackfleishman@outlook.com>
2020-05-29 15:26:56 -0400

    better movement i guess

--- Player.gd
+++ Player.gd
@@ -2,18 +2,19 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 export var left = true
-export var acceleration = .2
-export var max_speed = 200
-export var friction = .1
-export var gravity = 20
-export var jump_power = -500
+const acceleration = 512
+const max_speed = 64
+const friction = .02
+const ground_friction = .1
+const gravity = 512
+const jump_power = -256
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var prefix
 	if left:
 		prefix = "left_"
@@ -21,12 +22,13 @@ func _physics_process(_delta):
 		prefix = "right_"
 	var input = (Input.get_action_strength(prefix + "right") - Input.get_action_strength(prefix + "left"))
 	if input != 0:
-		velocity.x = lerp(velocity.x, input * max_speed, acceleration)
+		velocity.x += clamp(input * acceleration * delta, -max_speed, max_speed)
+	elif is_on_floor():
+		velocity.x = lerp(velocity.x, 0, ground_friction)
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
 	if Input.is_action_just_pressed(prefix + "up") and is_on_floor():
 		velocity.y = jump_power
 	else:
-		velocity.y += gravity
-	print(is_on_floor())
+		velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
